25188d7c2cef4f16a6a48bece35b113904c3bc8a
Joel Barbosa <joelbarbosa@joel-2.local>
2023-10-09 21:07:55 -0300

    update list exp

--- src/list.cpp
+++ src/list.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <list>
 
+// using namespace std;
+
 int main() {
     std::list<int> myList;
 
@@ -23,9 +25,35 @@ int main() {
     {
         std::cerr << "error is: " << e.what() << '\n';
     }
-    
 
     std::cout << std::endl;
+
+    myList.pop_front();
+    myList.pop_back();
+
+    for (int element: myList) {
+        std::cout << element << " ";
+    }
+
+    std::cout << std::endl;
+
+    auto it = myList.begin();
+    ++it;
+    myList.insert(it, 5);
+
+    auto it2 = myList.end();
+    myList.insert(it2, 8);
+
+    for (int element: myList) {
+        std::cout << element << " ";
+    }
+
+    std::cout << std::endl;
+
+    std::cout << "Size :" << myList.size() << std::endl;
+
+    myList.clear();
+    std::cout << "Size :" << myList.size() << std::endl;
 
     return 0;
 }
